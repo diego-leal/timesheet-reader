b5bb522166aa24844c995a5500750e90c7ce5428
Diego Leal <leal.banks@gmail.com>
2019-10-20 00:41:00 -0300

    feat: implements report method

--- lib/timesheet_reader/formatter.rb
+++ lib/timesheet_reader/formatter.rb
@@ -1,4 +1,25 @@
 module TimesheetReader
     class Formatter
+        def report (timesheets, total_hours, total_minutes)
+            timesheets.each do |timesheet|
+                filename = timesheet[:filename]
+                hours = timesheet[:hours]
+                minutes = timesheet[:minutes]
+
+                print_timesheet_line(filename, hours, minutes)
+            end
+
+            puts "Total hours: #{format_time(total_hours)}:#{format_time(total_minutes)}"
+        end
+
+        private
+
+        def format_time (time)
+            time < 10 ? "0" << time.to_s : time.to_s
+        end
+
+        def print_timesheet_line (filename, hours, minutes)
+            puts "#{filename}: #{format_time(hours)}:#{format_time(minutes)} hours"
+        end
     end
 end
