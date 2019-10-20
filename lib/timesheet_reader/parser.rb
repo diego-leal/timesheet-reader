d3152cabea7bcce6aba0333f396c57df466ff430
Diego Leal <leal.banks@gmail.com>
2019-10-20 00:40:16 -0300

    feat: add validation for time values

--- lib/timesheet_reader/parser.rb
+++ lib/timesheet_reader/parser.rb
@@ -17,10 +17,7 @@ module TimesheetReader
             file = read_file(relative_path)
             lines = build_lines(file[:lines])
 
-            return {
-                filename: file[:filename],
-                lines: lines
-            }
+            { filename: file[:filename], lines: lines }
         end
 
         private
@@ -30,36 +27,47 @@ module TimesheetReader
             path, filename = File.split(relative_path)
             lines = File.read(absolute_path).squeeze("\n").split("\n")
 
-            return {
-                filename: filename,
-                lines: lines
-            }
+            { filename: filename, lines: lines }
         end
 
         def parse_statements_block (lines)
             token_index = lines.index { |line| line.downcase == "#{@token} #{@keyword}" }
             start_of_block = token_index + 1
             end_of_block = lines[start_of_block..].index { |line| line.include?("#{@token}") }
-            lines_range = lines[start_of_block..end_of_block]
-
-            return lines_range
+            lines[start_of_block..end_of_block]
         end
 
         def build_lines (lines)
             parsed_lines = parse_statements_block(lines)
-            sanitized_lines = []
 
-            parsed_lines.each do |line|
+            validate_timesheet_intervals(parsed_lines)
+
+            parsed_lines.map do |line|
                 statement, hours, minutes = line.split(":")
 
-                sanitized_lines.push({
-                    statement: statement,
-                    hours: hours.to_i,
-                    minutes: minutes.to_i
-                })
-            end
+                validate_time_sheet_hours(hours)
+                validate_time_sheet_minutes(minutes)
 
-            return sanitized_lines
+                {
+                    statement: statement,
+                    hours: hours.to_i == 0 ? 24 : hours.to_i,
+                    minutes: minutes.to_i
+                }
+            end
+        end
+
+        def validate_timesheet_intervals (lines)
+            raise ParserError.new("You should close the interval of last line") if lines.length.odd?
+        end
+
+        def validate_time_sheet_hours (hours)
+            hours = hours.to_i
+            raise ParserError.new("You should use a number between 0-23 to define hours") if hours < 0 || hours > 23
+        end
+
+        def validate_time_sheet_minutes (minutes)
+            minutes = minutes.to_i
+            raise ParserError.new("You should use a number between 0-59 to define minutes") if minutes < 0 || minutes > 59
         end
     end
 end
